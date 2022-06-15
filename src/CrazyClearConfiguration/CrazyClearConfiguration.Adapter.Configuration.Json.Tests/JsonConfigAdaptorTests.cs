70abc83c2cb108b67819e210678501d709875d18
Robson <sc.robson@gmail.com>
2022-06-15 12:37:37 +0200

    Add some tests for reading and writing the json files

--- src/CrazyClearConfiguration/CrazyClearConfiguration.Adapter.Configuration.Json.Tests/JsonConfigAdaptorTests.cs
+++ src/CrazyClearConfiguration/CrazyClearConfiguration.Adapter.Configuration.Json.Tests/JsonConfigAdaptorTests.cs
@@ -6,28 +6,32 @@ namespace CrazyClearConfiguration.Adapter.Configuration.Json.Tests
     public class JsonConfigAdaptorTests
     {
         [Fact]
-        public async void CanReadJson()
+        public async void CanReadSimpleValuesFromJson()
         {
             var adaptor = new JsonConfigAdaptor("TestSettings.json");
             var expandoObject = await adaptor.Read();
 
-            foreach (var item in expandoObject)
-            {
-                switch (item.Key)
-                {
-                    case "String":
-                        Assert.Equal("Hello 1", item.Value.AsString());
-                        break;
-                    case "Int":
-                        Assert.Equal(1, item.Value.AsInt());
-                        break;
-                    case "Boolean":
-                        Assert.True(item.Value.AsBoolean());
-                        break;
-                    default:
-                        break;
-                }
-            }
+            Assert.Equal("Hello 1", expandoObject.GetValue("String").AsString());
+            Assert.Equal(1, expandoObject.GetValue("Int").AsInt());
+            Assert.True(expandoObject.GetValue("Boolean").AsBoolean());
+        }
+
+        [Fact]
+        public async void CanWriteSimpleValuesToJson()
+        {
+            var adaptor = new JsonConfigAdaptor("TestSettings.json");
+
+            dynamic expandoObjectBefore = await adaptor.Read();
+
+            expandoObjectBefore.String = "Update String";
+            expandoObjectBefore.NewString = "New String";
+
+            await adaptor.Write(expandoObjectBefore);
+
+            var expandoObjectAfter = await adaptor.Read();
+
+            Assert.Equal("Update String", expandoObjectAfter.GetValue("String").AsString());
+            Assert.Equal("New String", expandoObjectAfter.GetValue("NewString").AsString());
         }
     }
 }
---
+++ src/CrazyClearConfiguration/CrazyClearConfiguration.Core/Extensions/ExpandoObjectExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Dynamic;
+
+namespace CrazyClearConfiguration.Core.Extensions
+{
+    public static class ExpandoObjectExtensions
+    {
+        public static object? GetValue(this ExpandoObject expandoObject, string key)
+        {
+            return expandoObject.Single(x => x.Key == key).Value;
+        }
+    }
+}
