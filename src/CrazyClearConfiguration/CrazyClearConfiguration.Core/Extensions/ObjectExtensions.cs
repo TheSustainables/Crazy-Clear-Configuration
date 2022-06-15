35eddce4425f73b9433857b4279a552e8e5246d9
Robson <sc.robson@gmail.com>
2022-06-15 15:14:03 +0200

    Remove unused extensions

--- src/CrazyClearConfiguration/CrazyClearConfiguration.Core/Extensions/ObjectExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CrazyClearConfiguration.Core.Extensions
-{
-    public static class ObjectExtensions
-    {
-        public static string AsString(this object? obj) => obj?.ToString() ?? string.Empty;
-        public static int AsInt(this object? obj) => int.Parse(obj?.ToString() ?? "0");
-        public static bool AsBoolean(this object? obj) => bool.Parse(obj?.ToString() ?? "false");
-    }
-}
